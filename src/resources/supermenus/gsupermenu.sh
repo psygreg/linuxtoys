c63691b5fb6f9a154d19867afab7eb937c57b26a
Victor <psygreg@pm.me>
2025-06-20 13:18:49 -0300

    add Osu! on Flathub to gaming menu

--- src/resources/supermenus/gsupermenu.sh
+++ src/resources/supermenus/gsupermenu.sh
@@ -26,6 +26,7 @@ gsupermenu () {
     local mcbe_status=$([ "$_mcbe" = "io.mrarm.mcpelauncher" ] && echo "ON" || echo "OFF")
     local runner_status=$([ "$_runner" = "runners" ] && echo "ON" || echo "OFF")
     local nexmod_status=$([ "$_nexmod" = "yes" ] && echo "ON" || echo "OFF")
+    local osuf_status=$([ "$_osuf" = "sh.ppy.osu" ] && echo "ON" || echo "OFF")
 
     while :; do
 
@@ -39,6 +40,7 @@ gsupermenu () {
             "SteamTinkerLaunch" "$msg113" $stl_status \
             "NexusMods" "$msg189" $nexmod_status \
             "Sober" "$msg114" $sober_status \
+            "Osu!" "$msg221" $osuf_status \
             "Bedrock Launcher" "$msg160" $mcbe_status \
             "Discord" "$msg130" $disc_status \
             "Gamemode" "$msg115" $gmode_status \
@@ -65,6 +67,7 @@ gsupermenu () {
         [[ "$selection" == *"ProtonPlus"* ]] && _pp="com.vysp3r.ProtonPlus" || _pp=""
         [[ "$selection" == *"SteamTinkerLaunch"* ]] && _stl="com.valvesoftware.Steam.Utility.steamtinkerlaunch" || _stl=""
         [[ "$selection" == *"Sober"* ]] && _sobst="org.vinegarhq.Sober" || _sobst=""
+        [[ "$selection" == *"Osu!"* ]] && _osuf="sh.ppy.osu" || _osuf=""
         [[ "$selection" == *"Bedrock Launcher"* ]] && _mcbe="io.mrarm.mcpelauncher" || _mcbe=""
         [[ "$selection" == *"Discord"* ]] && _disc="com.discordapp.Discord" || _gmode=""
         [[ "$selection" == *"Gamemode"* ]] && _gmode="gamemode" || _gmode=""
@@ -132,7 +135,7 @@ install_native () {
 # flatpak packages
 install_flatpak () {
 
-    local _flatpaks=($_lutris $_heroic $_pp $_stl $_sobst $_disc $_wivrn $_steer $_mcbe)
+    local _flatpaks=($_lutris $_heroic $_pp $_stl $_sobst $_disc $_wivrn $_steer $_mcbe $_osuf)
     if [[ -n "$_flatpaks" ]] || [[ -n "$_steam" ]] || [[ -n "$_gfn" ]]; then
         if command -v flatpak &> /dev/null; then
             flatpak_in_lib
