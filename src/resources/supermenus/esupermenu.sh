a130b0b5ccf71194f993dc4d76da59c4d2886a23
Victor <psygreg@pm.me>
2025-06-26 09:30:59 -0300

    add CachyOS config files to prebuilt kernel install

--- src/resources/supermenus/esupermenu.sh
+++ src/resources/supermenus/esupermenu.sh
@@ -301,7 +301,7 @@ kernel_in () {
                 cd $HOME
                 wget "https://github.com/psygreg/linux-cachyos-deb/archive/refs/tags/linux-headers-psycachy_${psycachy_tag}-1_amd64.deb"
                 wget "https://github.com/psygreg/linux-cachyos-deb/archive/refs/tags/linux-image-psycachy_${psycachy_tag}-1_amd64.deb"
-                sudo dpkg -i -y linux-image-psycachy_${psycachy_tag}-1_amd64.deb linux-headers-psycachy_${psycachy_tag}-1_amd64.deb
+                sudo dpkg -i -y linux-image-psycachy_${psycachy_tag}-1_amd64.deb linux-headers-psycachy_${psycachy_tag}-1_amd64.deb || exit 10
                 cd $HOME/.local
                 sleep 1
                 wget -O "kernelsetting" https://raw.githubusercontent.com/psygreg/linuxtoys/refs/heads/main/src/kernelsetting-defaults
@@ -309,6 +309,51 @@ kernel_in () {
                 sleep 1
                 rm linux-image-psycachy_${psycachy_tag}-1_amd64.deb
                 rm linux-headers-psycachy_${psycachy_tag}-1_amd64.deb
+                # apply system config
+                local _cfgsource="https://raw.githubusercontent.com/CachyOS/CachyOS-Settings/master/usr"
+                mkdir -p sysctl-config
+                sleep 1
+                cd sysctl-config
+                {
+                    echo "${_cfgsource}/lib/udev/rules.d/20-audio-pm.rules"
+                    echo "${_cfgsource}/lib/udev/rules.d/40-hpet-permissions.rules"
+                    echo "${_cfgsource}/lib/udev/rules.d/50-sata.rules"
+                    echo "${_cfgsource}/lib/udev/rules.d/60-ioschedulers.rules"
+                    echo "${_cfgsource}/lib/udev/rules.d/69-hdparm.rules"
+                    echo "${_cfgsource}/lib/udev/rules.d/99-cpu-dma-latency.rules"
+                    } > "udev.txt"
+                {
+                    echo "${_cfgsource}/lib/tmpfiles.d/coredump.conf"
+                    echo "${_cfgsource}/lib/tmpfiles.d/thp-shrinker.conf"
+                    echo "${_cfgsource}/lib/tmpfiles.d/thp.conf"
+                    } > "tmpfiles.txt"
+                {
+                    echo "${_cfgsource}/lib/modprobe.d/20-audio-pm.conf"
+                    echo "${_cfgsource}/lib/modprobe.d/amdgpu.conf"
+                    echo "${_cfgsource}/lib/modprobe.d/blacklist.conf"
+                    } > "modprobe.txt"
+                {
+                    echo "${_cfgsource}/lib/NetworkManager/conf.d/dns.conf"
+                    echo "${_cfgsource}/lib/sysctl.d/99-cachyos-settings.conf"
+                    echo "${_cfgsource}/lib/systemd/journald.conf.d/00-journal-size.conf"
+                    echo "${_cfgsource}/share/X11/xorg.conf.d/20-touchpad.conf"
+                    } > "other.txt"
+                sleep 1
+                while read -r url; do wget -P udev "$url"; done < udev.txt
+                while read -r url; do wget -P tmpfiles "$url"; done < tmpfiles.txt
+                while read -r url; do wget -P modprobe "$url"; done < modprobe.txt
+                while read -r url; do wget "$url"; done < other.txt
+                sleep 1
+                sudo cp -rf udev/* /usr/lib/udev/rules.d/
+                sudo cp -rf tmpfiles/* /usr/lib/tmpfiles.d/
+                sudo cp -rf modprobe/* /usr/lib/modprobe.d/
+                sudo cp -f dns.conf /usr/lib/NetworkManager/conf.d/
+                sudo cp -f 99-cachyos-settings.conf /usr/lib/sysctl.d/
+                sudo cp -f 00-journal-size.conf /usr/lib/systemd/journald.conf.d/
+                sudo cp -f 20-touchpad.conf /usr/share/X11/xorg.conf.d/
+                cd ..
+                rm -rf sysctl-config
+                # sign kernel image for secure boot
                 if sudo mokutil --sb-state | grep -q "SecureBoot enabled"; then
                     wget https://raw.githubusercontent.com/psygreg/linux-cachyos-deb/refs/heads/master/secureboot/create-key.sh
                     chmod +x create-key.sh
