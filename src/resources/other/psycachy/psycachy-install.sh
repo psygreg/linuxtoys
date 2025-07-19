6bacfa868c45db95fc6782a0c33db53b6475df5c
Victor <psygreg@pm.me>
2025-07-19 15:47:10 -0300

    fix failure to get version numbers on kernel installer

--- src/resources/other/psycachy/psycachy-install.sh
+++ src/resources/other/psycachy/psycachy-install.sh
@@ -2,8 +2,8 @@
 releases=$(curl -s "https://api.github.com/repos/psygreg/linux-psycachy/releases")
 lts_tag=$(echo "$releases" | jq -r '.[].tag_name' | grep -i '^LTS-' | sort -Vr | head -n 1)
 std_tag=$(echo "$releases" | jq -r '.[].tag_name' | grep -i '^STD-' | sort -Vr | head -n 1)
-kver_lts="${lts_tag#LTS-}"
-kver_psycachy="${std_tag#STD-}"
+kver_lts=$(echo "$lts_tag" | cut -d'-' -f2-)
+kver_psycachy=$(echo "$std_tag" | cut -d'-' -f2-)
 _kv_url_latest=$(curl -s https://www.kernel.org | grep -A 1 'id="latest_link"' | awk 'NR==2' | grep -oP 'href="\K[^"]+')
 # extract only the version number
 _kv_latest=$(echo $_kv_url_latest | grep -oP 'linux-\K[^"]+')
@@ -13,21 +13,20 @@ _kv_latest=$(basename $_kv_latest .tar.xz)
 # psycachy standard edition
 psycachy_std () {
 
-    local psycachy_tag="$kver_psycachy"
     cd $HOME
-    wget "https://github.com/psygreg/linux-cachyos-deb/releases/download/${psycachy_tag}/linux-headers-psycachy_${psycachy_tag}-1_amd64.deb"
-    wget "https://github.com/psygreg/linux-cachyos-deb/releases/download/${psycachy_tag}/linux-image-psycachy_${psycachy_tag}-1_amd64.deb"
-    wget "https://github.com/psygreg/linux-cachyos-deb/releases/download/${psycachy_tag}/linux-libc-dev_${psycachy_tag}-1_amd64.deb"
+    wget "https://github.com/psygreg/linux-psycachy/releases/download/${std_tag}/linux-headers-psycachy_${kver_psycachy}-1_amd64.deb"
+    wget "https://github.com/psygreg/linux-psycachy/releases/download/${std_tag}/linux-image-psycachy_${kver_psycachy}-1_amd64.deb"
+    wget "https://github.com/psygreg/linux-psycachy/releases/download/${std_tag}/linux-libc-dev_${kver_psycachy}-1_amd64.deb"
     sleep 1
-    sudo dpkg -i linux-image-psycachy_${psycachy_tag}-1_amd64.deb linux-headers-psycachy_${psycachy_tag}-1_amd64.deb linux-libc-dev_${psycachy_tag}-1_amd64.deb || exit 10
+    sudo dpkg -i linux-image-psycachy_${kver_psycachy}-1_amd64.deb linux-headers-psycachy_${kver_psycachy}-1_amd64.deb linux-libc-dev_${kver_psycachy}-1_amd64.deb || exit 10
     cd $HOME/.local
     sleep 1
     wget -O "kernelsetting" https://raw.githubusercontent.com/psygreg/linuxtoys/refs/heads/main/src/kernelsetting-defaults
     cd $HOME
     sleep 1
-    rm linux-image-psycachy_${psycachy_tag}-1_amd64.deb
-    rm linux-headers-psycachy_${psycachy_tag}-1_amd64.deb
-    rm linux-libc-dev_${psycachy_tag}-1_amd64.deb
+    rm linux-image-psycachy_${kver_psycachy}-1_amd64.deb
+    rm linux-headers-psycachy_${kver_psycachy}-1_amd64.deb
+    rm linux-libc-dev_${kver_psycachy}-1_amd64.deb
     # sign kernel image for secure boot
     if sudo mokutil --sb-state | grep -q "SecureBoot enabled"; then
         bash <(curl -s https://raw.githubusercontent.com/psygreg/linux-cachyos-deb/refs/heads/master/secureboot/create-key.sh) --linuxtoys
@@ -38,21 +37,20 @@ psycachy_std () {
 # psycachy lts edition
 psycachy_lts () {
 
-    local psycachy_tag="$kver_lts"
     cd $HOME
-    wget "https://github.com/psygreg/linux-cachyos-deb/releases/download/${psycachy_tag}/linux-headers-psycachy_${psycachy_tag}-1_amd64.deb"
-    wget "https://github.com/psygreg/linux-cachyos-deb/releases/download/${psycachy_tag}/linux-image-psycachy_${psycachy_tag}-1_amd64.deb"
-    wget "https://github.com/psygreg/linux-cachyos-deb/releases/download/${psycachy_tag}/linux-libc-dev_${psycachy_tag}-1_amd64.deb"
+    wget "https://github.com/psygreg/linux-cachyos-deb/releases/download/${lts_tag}/linux-headers-psycachy_${kver_lts}-1_amd64.deb"
+    wget "https://github.com/psygreg/linux-cachyos-deb/releases/download/${lts_tag}/linux-image-psycachy_${kver_lts}-1_amd64.deb"
+    wget "https://github.com/psygreg/linux-cachyos-deb/releases/download/${lts_tag}/linux-libc-dev_${kver_lts}-1_amd64.deb"
     sleep 1
-    sudo dpkg -i linux-image-psycachy-lts_${psycachy_tag}-1_amd64.deb linux-headers-psycachy-lts_${psycachy_tag}-1_amd64.deb linux-libc-dev_${psycachy_tag}-1_amd64.deb || exit 10
+    sudo dpkg -i linux-image-psycachy-lts_${kver_lts}-1_amd64.deb linux-headers-psycachy-lts_${kver_lts}-1_amd64.deb linux-libc-dev_${kver_lts}-1_amd64.deb || exit 10
     cd $HOME/.local
     sleep 1
     wget -O "kernelsetting-lts" https://raw.githubusercontent.com/psygreg/linuxtoys/refs/heads/main/src/kernelsetting-defaults
     cd $HOME
     sleep 1
-    rm linux-image-psycachy-lts_${psycachy_tag}-1_amd64.deb
-    rm linux-headers-psycachy-lts_${psycachy_tag}-1_amd64.deb
-    rm linux-libc-dev_${psycachy_tag}-1_amd64.deb
+    rm linux-image-psycachy-lts_${kver_lts}-1_amd64.deb
+    rm linux-headers-psycachy-lts_${kver_lts}-1_amd64.deb
+    rm linux-libc-dev_${kver_lts}-1_amd64.deb
     # sign kernel image for secure boot
     if sudo mokutil --sb-state | grep -q "SecureBoot enabled"; then
         bash <(curl -s https://raw.githubusercontent.com/psygreg/linux-cachyos-deb/refs/heads/master/secureboot/create-key.sh) --lts
