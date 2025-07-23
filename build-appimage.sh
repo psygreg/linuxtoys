dd24fc39f2f6d78ce4925bbed0faac1d80001505
Victor <psygreg@pm.me>
2025-07-23 11:27:01 -0300

    improve appimage build process

--- build-appimage.sh
+++ build-appimage.sh
@@ -2,8 +2,12 @@
 
 # ensure correct working directory
 cd "$(dirname "$(realpath "$0")")"
+# make directory structure
+mkdir -p appimagebuild/LinuxToys.AppDir/usr/bin
+mkdir -p appimagebuild/LinuxToys.AppDir/usr/lib
+
 # get updated LinuxToys and set proper filename
-cp src/linuxtoys.sh linuxtoys1.sh
+cp linuxtoys.sh linuxtoys1.sh
 mv linuxtoys1.sh linuxtoys
 mv -f linuxtoys appimagebuild/LinuxToys.AppDir/usr/bin/
 
@@ -15,5 +19,11 @@ for bin in curl wget git whiptail bash; do
         cp -u --parents "$dep" appimagebuild/LinuxToys.AppDir/usr/lib/;
     done;
 done
+
+# adjust library dir structure
+mv appimagebuild/LinuxToys.AppDir/usr/lib/lib/x86_64-linux-gnu appimagebuild/LinuxToys.AppDir/usr/
+rm -r appimagebuild/LinuxToys.AppDir/usr/lib
+mv appimagebuild/LinuxToys.AppDir/usr/x86_64-linux-gnu appimagebuild/LinuxToys.AppDir/usr/lib
+
 # build appimage
 ./appimagebuild/appimagetool-x86_64.AppImage appimagebuild/LinuxToys.AppDir
