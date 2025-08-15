a569f49453ca5bb3ec991067c9d6a03a6d3c79ec
Victor <psygreg@pm.me>
2025-08-14 22:34:26 -0300

    add prompter to btrfs-assistant installer

--- p3/scripts/extra/btassist.sh
+++ p3/scripts/extra/btassist.sh
@@ -13,6 +13,7 @@ _lang_
 source "$SCRIPT_DIR/../../libs/lang/${langfile}.lib"
 source "$SCRIPT_DIR/../../libs/helpers.lib"
 if [ "$(findmnt -n -o FSTYPE /)" = "btrfs" ]; then
+    sudo_rq
     if [[ "$ID" =~ "arch" ]] || [[ "$ID" != "cachyos" ]] && [[ "$ID_LIKE" == *arch* ]] || [[ "$ID" != "cachyos" ]] && [[ "$ID_LIKE" == *archlinux* ]]; then
         if zenity --question --text "$msg035" --width 360 --height 300; then
             chaotic_aur_lib
