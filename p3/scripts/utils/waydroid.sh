2c3bf0f276368780698d46528d3d4a7e41cdc408
Victor <psygreg@pm.me>
2025-08-17 00:13:21 -0300

    add initialization of Android image and optional ARM translation layer to Waydroid installer script

--- p3/scripts/utils/waydroid.sh
+++ p3/scripts/utils/waydroid.sh
@@ -9,6 +9,8 @@
 . /etc/os-release
 SCRIPT_DIR="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
 source "$SCRIPT_DIR/../../libs/linuxtoys.lib"
+_lang_
+source "$SCRIPT_DIR/../../libs/lang/${langfile}.lib"
 if [ "$XDG_SESSION_TYPE" = "wayland" ]; then
     sudo_rq
     if [[ "$ID_LIKE" == *debian* ]] || [[ "$ID_LIKE" == *ubuntu* ]] || [ "$ID" == "debian" ] || [ "$ID" == "ubuntu" ]; then
@@ -19,6 +21,29 @@ if [ "$XDG_SESSION_TYPE" = "wayland" ]; then
     _packages=(waydroid)
     _install_
     sudo systemctl enable --now waydroid-container
+    sudo waydroid init --system-url https://ota.waydro.id/system_gapps --vendor-url https://ota.waydro.id/vendor_gapps
+    if zenity --question --title="Waydroid" --text="$msg283" --width 300 --height 300; then
+        waydroid session stop
+        sudo waydroid container stop
+        cd $HOME
+        git clone https://github.com/casualsnek/waydroid_script
+        cd waydroid_script
+        _packages=(pip)
+        _install_
+        python3 -m venv venv
+        venv/bin/pip install -r requirements.txt
+        # Detect CPU vendor for proper ARM translation layer
+        CPU_VENDOR=$(grep -m1 'vendor_id' /proc/cpuinfo | awk '{print $3}')
+        if [[ "$CPU_VENDOR" == "GenuineIntel" ]]; then
+            sudo venv/bin/python3 main.py install libhoudini
+        elif [[ "$CPU_VENDOR" == "AuthenticAMD" ]]; then
+            sudo venv/bin/python3 main.py install libndk
+        fi
+        cd ..
+        rm -r waydroid_script
+    fi
+    zeninf "$msg284"
+    xdg-open https://docs.waydro.id/faq/google-play-certification
 else
     fatal "$msg219"
     exit 1
