6c08e5b9693a1bb87d170ad91bf717b1a614b8e9
jeiel0rbit <jeiel.lima.miranda@gmail.com>
2025-08-20 11:43:15 -0300

    feat: add sublimetxt

---
+++ p3/scripts/devs/sublimetext.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+# name: Sublime Text
+# version: 4
+# description: sublime_desc
+# icon: sublime.svg
+
+# --- Start of the script code ---
+. /etc/os-release
+SCRIPT_DIR="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
+source "$SCRIPT_DIR/../../libs/linuxtoys.lib"
+_lang_
+source "$SCRIPT_DIR/../../libs/lang/${langfile}.lib"
+source "$SCRIPT_DIR/../../libs/helpers.lib"
+sudo_rq
+
+# Instalação para Debian e Ubuntu
+if [[ "$ID_LIKE" == *debian* ]] || [[ "$ID_LIKE" == *ubuntu* ]] || [ "$ID" == "debian" ] || [ "$ID" == "ubuntu" ]; then
+    wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/sublimehq-archive.gpg > /dev/null
+    echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+    sudo apt-get update
+    _packages=(sublime-text)
+    _install_
+
+# Instalação para Fedora e derivados
+elif [[ "$ID_LIKE" =~ (rhel|fedora) ]] || [[ "$ID" =~ (fedora) ]]; then
+    sudo rpm -v --import https://download.sublimetext.com/sublimehq-rpm-pub.gpg
+    sudo dnf config-manager --add-repo https://download.sublimetext.com/rpm/stable/x86_64/sublime-text.repo
+    _packages=(sublime-text)
+    _install_
+
+# Instalação para Arch Linux e derivados
+elif [[ "$ID" =~ ^(arch|cachyos)$ ]] || [[ "$ID_LIKE" == *arch* ]] || [[ "$ID_LIKE" == *archlinux* ]]; then
+    curl -O https://download.sublimetext.com/sublimehq-pub.gpg && sudo pacman-key --add sublimehq-pub.gpg && sudo pacman-key --lsign-key 8A8F901A && rm sublimehq-pub.gpg
+    echo -e "\n[sublime-text]\nServer = https://download.sublimetext.com/arch/stable/x86_64" | sudo tee -a /etc/pacman.conf
+    sudo pacman -Syu
+    _packages=(sublime-text)
+    _install_
+
+else
+    nonfatal "$msg077" # Mensagem de "Sistema operacional não compatível"
+fi
+
+zeninf "$msg018" # Mensagem de "Operações concluídas."
