5ce9530e5fa03944193e7e34dfa8f7b0c3b18a91
Victor <psygreg@pm.me>
2025-08-20 10:06:46 -0300

    added bottles, improved menu look and hover effect

--- p3/app/window.py
+++ p3/app/window.py
@@ -482,8 +482,8 @@ class AppWindow(Gtk.ApplicationWindow):
             .script-item-hover {
                 background-color: rgba(255, 255, 255, 0.05);
                 border: 1px solid rgba(255, 255, 255, 0.9);
-                border-radius: 8px;
-                box-shadow: 0 2px 8px rgba(0, 0, 0, 0.2);
+                border-radius: 16px;
+                box-shadow: 0 0px 0px rgba(0, 0, 0, 0.2);
                 transition: all 200ms ease-in-out;
             }
             """
