5782005012ddb2477fa0fcb34df61097aaab02e6
Victor <psygreg@pm.me>
2025-08-22 20:11:56 -0300

    fix #94

--- p3/scripts/drivers/nv-deb.sh
+++ p3/scripts/drivers/nv-deb.sh
@@ -14,4 +14,4 @@ wget https://developer.download.nvidia.com/compute/cuda/repos/debian12/x86_64/cu
 sudo dpkg -i cuda-keyring_1.1-1_all.deb
 sleep 1
 sudo apt-get update
-sudo apt-get install cuda-drivers
+sudo apt-get install -y cuda-drivers
