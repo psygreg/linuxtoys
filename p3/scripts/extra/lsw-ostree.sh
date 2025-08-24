3eef1c7eba98d97427f3778cdb8865ef8079ab77
Victor <psygreg@pm.me>
2025-08-24 00:33:22 -0300

    add sudo request to install shortcuts (timeout)

--- p3/scripts/extra/lsw-ostree.sh
+++ p3/scripts/extra/lsw-ostree.sh
@@ -101,7 +101,8 @@ win_install () {
 lsw_install () {
 	if zenity --question --title "Setup" --text "Is the Windows installation finished?" --height=300 --width=300; then
 		wget https://raw.githubusercontent.com/psygreg/linuxtoys-atom/refs/heads/main/lsw-atom/rpmbuild/RPMS/x86_64/lsw-atom-shortcuts-1.1-1.x86_64.rpm
-		sudo rpm-ostree install -yA lsw-atom-shortcuts-1.1-1.x86_64.rpm
+		sudo_rq
+        sudo rpm-ostree install -yA lsw-atom-shortcuts-1.1-1.x86_64.rpm
         rm lsw-atom-shortcuts-1.1-1.x86_64.rpm
 		exit 0
 	else
