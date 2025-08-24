220cb7404ecfbacceb8ac97241ef7b1993a1cff3
Victor <psygreg@pm.me>
2025-08-24 20:21:38 -0300

    make godot installers auto-update

--- p3/scripts/devs/godot.sh
+++ p3/scripts/devs/godot.sh
@@ -11,27 +11,35 @@ source "$SCRIPT_DIR/../../libs/linuxtoys.lib"
 _lang_
 source "$SCRIPT_DIR/../../libs/lang/${langfile}.lib"
 cd $HOME
+get_latest_godot_url() {
+    curl -s https://api.github.com/repos/godotengine/godot/releases/latest | \
+    grep browser_download_url | grep 'stable_linux.x86_64.zip' | head -n 1 | cut -d '"' -f 4
+}
+GODOT_URL=$(get_latest_godot_url)
+GODOT_ZIP="Godot_latest_linux.x86_64.zip"
 # first install
 if [ ! -d "$HOME/.local/godot" ]; then
-    wget 'https://objects.githubusercontent.com/github-production-release-asset-2e65be/15634981/5c13b07c-aad3-4bde-8712-9f0825758bb2?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=releaseassetproduction%2F20250602%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20250602T210343Z&X-Amz-Expires=300&X-Amz-Signature=2b5d1d411f853ce8c1eb9045af1b02f3567a4a8de13d754a3f1b3fce345a0051&X-Amz-SignedHeaders=host&response-content-disposition=attachment%3B%20filename%3DGodot_v4.4.1-stable_linux.x86_64.zip&response-content-type=application%2Foctet-stream'
-    unzip Godot_v4.4.1-stable_linux.x86_64.zip
-    mv Godot_v4.4.1-stable_linux.x86_64 Godot
-    mkdir -p $HOME/.local/godot
-    cp Godot -f $HOME/.local/godot
+    wget "$GODOT_URL" -O "$GODOT_ZIP"
+    unzip "$GODOT_ZIP"
+    GODOT_BIN=$(ls Godot*_linux.x86_64)
+    mv "$GODOT_BIN" Godot
+    mkdir -p "$HOME/.local/godot"
+    cp Godot -f "$HOME/.local/godot"
     wget https://raw.githubusercontent.com/psygreg/linuxtoys/refs/heads/main/resources/godot/godot.png
-    cp godot.png $HOME/.local/godot
+    cp godot.png "$HOME/.local/godot"
     rm Godot
     rm godot.png
-    rm Godot_v4.4.1-stable_linux.x86_64.zip
+    rm "$GODOT_ZIP"
     wget https://raw.githubusercontent.com/psygreg/linuxtoys/refs/heads/main/resources/godot/godot.desktop
-    cp godot.desktop $HOME/.local/share/applications
+    cp godot.desktop "$HOME/.local/share/applications/"
     rm godot.desktop
 else # update
-    wget 'https://objects.githubusercontent.com/github-production-release-asset-2e65be/15634981/5c13b07c-aad3-4bde-8712-9f0825758bb2?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=releaseassetproduction%2F20250602%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20250602T210343Z&X-Amz-Expires=300&X-Amz-Signature=2b5d1d411f853ce8c1eb9045af1b02f3567a4a8de13d754a3f1b3fce345a0051&X-Amz-SignedHeaders=host&response-content-disposition=attachment%3B%20filename%3DGodot_v4.4.1-stable_linux.x86_64.zip&response-content-type=application%2Foctet-stream'
-    unzip Godot_v4.4.1-stable_linux.x86_64.zip
-    mv Godot_v4.4.1-stable_linux.x86_64 Godot
-    cp Godot -f $HOME/.local/godot
+    wget "$GODOT_URL" -O "$GODOT_ZIP"
+    unzip "$GODOT_ZIP"
+    GODOT_BIN=$(ls Godot*_linux.x86_64)
+    mv "$GODOT_BIN" Godot
+    cp Godot -f "$HOME/.local/godot"
     rm Godot
-    rm Godot_v4.4.1-stable_linux.x86_64.zip
+    rm "$GODOT_ZIP"
 fi
 zeninf "$msg018"
--- p3/scripts/devs/godotsharp.sh
+++ p3/scripts/devs/godotsharp.sh
@@ -13,17 +13,24 @@ _lang_
 source "$SCRIPT_DIR/../../libs/lang/${langfile}.lib"
 cd $HOME
 if [[ "$ID_LIKE" =~ (rhel|fedora) || "$ID" =~ (fedora|ubuntu|debian) || "$NAME" == "openSUSE Leap" ]]; then
+    get_latest_godot_mono_url() {
+        curl -s https://api.github.com/repos/godotengine/godot/releases/latest | \
+        grep browser_download_url | grep 'stable_mono_linux_x86_64.zip' | head -n 1 | cut -d '"' -f 4
+    }
+    GODOT_MONO_URL=$(get_latest_godot_mono_url)
+    GODOT_MONO_ZIP="Godot_latest_mono_linux.x86_64.zip"
+
     # first install
     if [ ! -d "$HOME/.local/godot" ]; then
-        wget 'https://objects.githubusercontent.com/github-production-release-asset-2e65be/15634981/8976b3a0-fb60-4d98-bd70-b623b9eaf9d3?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=releaseassetproduction%2F20250602%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20250602T211639Z&X-Amz-Expires=300&X-Amz-Signature=11f42225a48cf9dea2a262ff4918e8c594b8494bd70ac108cf2e0b014fb8ac46&X-Amz-SignedHeaders=host&response-content-disposition=attachment%3B%20filename%3DGodot_v4.4.1-stable_mono_linux_x86_64.zip&response-content-type=application%2Foctet-stream'
+        wget "$GODOT_MONO_URL" -O "$GODOT_MONO_ZIP"
         mkdir -p godot
-        unzip -d $HOME/godot Godot_v4.4.1-stable_mono_linux.x86_64.zip
+        unzip -d $HOME/godot "$GODOT_MONO_ZIP"
         cp -rf godot "$HOME/.local/"
         wget https://raw.githubusercontent.com/psygreg/linuxtoys/refs/heads/main/resources/godot/godot.png
         cp godot.png "$HOME/.local/godot"
         rm -rf godot
         rm godot.png
-        rm Godot_v4.4.1-stable_mono_linux.x86_64.zip
+        rm "$GODOT_MONO_ZIP"
         wget https://raw.githubusercontent.com/psygreg/linuxtoys/refs/heads/main/resources/godot/godotsharp.desktop
         cp godotsharp.desktop "$HOME/.local/share/applications"
         rm godotsharp.desktop
@@ -43,13 +50,13 @@ if [[ "$ID_LIKE" =~ (rhel|fedora) || "$ID" =~ (fedora|ubuntu|debian) || "$NAME" 
         _packages=(dotnet-sdk-9.0)
         _install_
     else # update
-        wget 'https://objects.githubusercontent.com/github-production-release-asset-2e65be/15634981/8976b3a0-fb60-4d98-bd70-b623b9eaf9d3?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=releaseassetproduction%2F20250602%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20250602T211639Z&X-Amz-Expires=300&X-Amz-Signature=11f42225a48cf9dea2a262ff4918e8c594b8494bd70ac108cf2e0b014fb8ac46&X-Amz-SignedHeaders=host&response-content-disposition=attachment%3B%20filename%3DGodot_v4.4.1-stable_mono_linux_x86_64.zip&response-content-type=application%2Foctet-stream'
+        wget "$GODOT_MONO_URL" -O "$GODOT_MONO_ZIP"
         mkdir -p godot
-        unzip -d $HOME/godot Godot_v4.4.1-stable_mono_linux.x86_64.zip
+        unzip -d $HOME/godot "$GODOT_MONO_ZIP"
         rm -rf "$HOME/.local/godot"
         cp -rf godot "$HOME/.local/"
         rm -rf godot
-        rm Godot_v4.4.1-stable_mono_linux.x86_64.zip
+        rm "$GODOT_MONO_ZIP"
     fi
     zeninf "$msg018"
 else
