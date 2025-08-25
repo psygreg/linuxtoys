a1d04291c79ba3a787540ca69512d89125469059
Victor <psygreg@pm.me>
2025-08-25 10:05:09 -0300

    small code checks

--- p3/scripts/utils/privacy/wireguard.sh
+++ p3/scripts/utils/privacy/wireguard.sh
@@ -11,7 +11,6 @@ SCRIPT_DIR="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
 source "$SCRIPT_DIR/../../libs/linuxtoys.lib"
 _lang_
 source "$SCRIPT_DIR/../../libs/lang/${langfile}.lib"
-source "$SCRIPT_DIR/../../libs/helpers.lib"
 sudo_rq
 if [[ "$ID_LIKE" =~ (ubuntu|debian) ]];then
 	_packages=(wireguard)
@@ -21,7 +20,7 @@ elif [[ "$ID_LIKE" =~ (suse|rhel|fedora) ]];then
 	_install_
 elif [[ "$ID" =~ ^(arch|cachyos)$ ]] || [[ "$ID_LIKE" == *arch* ]] || [[ "$ID_LIKE" == *archlinux* ]]; then
 	_packages=(wireguard-tools)
-	_install_else
+	_install_
 else
     fatal "$msg077"
 fi
