153febe88ec7d3bffb337339c61effc5f05a06d5
Victor <psygreg@pm.me>
2025-08-26 13:41:13 -0300

    fix sudo_rq placement

--- p3/scripts/utils/bazaar.sh
+++ p3/scripts/utils/bazaar.sh
@@ -10,7 +10,7 @@ source "$SCRIPT_DIR/../../libs/linuxtoys.lib"
 _lang_
 source "$SCRIPT_DIR/../../libs/lang/${langfile}.lib"
 source "$SCRIPT_DIR/../../libs/helpers.lib"
-flatpak_in_lib
 sudo_rq
+flatpak_in_lib
 sudo flatpak install --or-update --system --noninteractive flathub io.github.kolunmi.Bazaar
 zeninf "$msg018"
