f75b1f093719619e8a77be8d6c832b97b3e21790
4ntsy <228124712+4ntsy@users.noreply.github.com>
2025-08-29 11:39:42 -0300

    feat: Analyzing manifest
    Signed-off-by: 4ntsy <228124712+4ntsy@users.noreply.github.com>

--- p3/app/head_menu.py
+++ p3/app/head_menu.py
@@ -1,7 +1,36 @@
 from .gtk_common import Gtk, GLib
 from . import cli_helper
 from . import script_runner
-import threading
+import threading, os
+
+
+class WaitDialog(Gtk.Dialog):
+	def __init__(self, parent, message="Waiting..."):
+		super().__init__(title="Waiting...", transient_for=parent, modal=True)
+		self.set_default_size(128, 48)
+		self.set_resizable(False)
+
+		box = self.get_content_area()
+		h = Gtk.Box(spacing=12)
+		h.set_border_width(12)
+		box.add(h)
+
+		self.spinner = Gtk.Spinner()
+		self.spinner.set_size_request(32, 32)
+		h.pack_start(self.spinner, False, False, 0)
+
+		label = Gtk.Label(label=message)
+		label.set_xalign(0)
+		h.pack_start(label, True, True, 0)
+
+		self.show_all()
+
+	def start(self):
+		self.spinner.start()
+		self.show_all()
+
+	def stop(self):
+		self.destroy()
 
 
 class MenuButton(Gtk.MenuButton):
@@ -9,6 +38,7 @@ class MenuButton(Gtk.MenuButton):
 		super().__init__()
 		self.script_runner = script_runner
 		self.results = []
+		self._temp_sh = '/tmp/._temp_script.sh'
 
 		_menu = Gtk.Menu()
 		_menu.set_halign(Gtk.Align.END)
@@ -19,7 +49,10 @@ class MenuButton(Gtk.MenuButton):
 		_menu.append(load_manifest)
 		_menu.show_all()
 
+		self.dlg = None
+
 		self.set_popup(_menu)
+
 
 	def __on_load_manifest(self, widget):
 		scripts_name = self.__file_choose()
@@ -47,17 +80,56 @@ class MenuButton(Gtk.MenuButton):
 
 		response = dialog.run()
 		if response == Gtk.ResponseType.OK:
+			self.dlg = WaitDialog(self.get_toplevel(), "Loading your manifest file... Please Wait...")
+			self.dlg.start()
 			scripts_name = cli_helper.load_manifest(dialog.get_filename())
 		dialog.destroy()
 
 		return scripts_name
 
 	def __wrapper_t(self, scripts_name):
+		packages_to_install = []
+		flatpaks_to_install = []
+
 		for script_name in scripts_name:
 			script_info = cli_helper.find_script_by_name(script_name)
-			script_info is not None and self.results.append(script_info)
+			if script_info is None:
+				if cli_helper.check_package_exists(script_name):
+					packages_to_install.append(script_name)
+					continue
+
+				elif cli_helper.check_flatpak_exists(script_name):
+					flatpaks_to_install.append(script_name)
+					continue
+			else:
+				self.results.append(script_info)
+
+		if packages_to_install or flatpaks_to_install:
+			self.results.append({'name': 'Packages & Flatpaks','path': self._temp_sh, 'is_script': True})
+			self.__temp_script(packages_to_install, flatpaks_to_install)
 
 		GLib.idle_add(self.__update_results)
 
 	def __update_results(self):
-		self.script_runner.run_scripts_sequentially(scripts_list=self.results)
+		self.dlg is not None and self.dlg.stop()
+
+		def completion_handler():
+			if os.path.exists(self._temp_sh):
+				os.remove(self._temp_sh)
+
+		self.script_runner.run_scripts_sequentially(scripts_list=self.results, on_completion=completion_handler)
+
+	def __temp_script(self, packages, flatpaks):
+		lib_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'libs', 'linuxtoys.lib')
+
+		script_content = f'''#!/bin/bash
+source "{lib_path}"
+
+_packages=("{' '.join(packages)}")
+_install_
+
+_flatpaks=("{' '.join(flatpaks)}")
+_flatpak_
+'''
+		with open(self._temp_sh, 'w+') as f:
+			f.write(script_content)
