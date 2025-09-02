7a0e57e386fe8b77a81b3f302e349f8884b09d6f
Victor <psygreg@pm.me>
2025-09-02 09:51:19 -0300

    fix missing sudo request

--- p3/scripts/extra/dsplitm.sh
+++ p3/scripts/extra/dsplitm.sh
@@ -14,6 +14,7 @@ source "$SCRIPT_DIR/../../libs/linuxtoys.lib"
 _lang_
 source "$SCRIPT_DIR/../../libs/lang/${langfile}.lib"
 source "$SCRIPT_DIR/../../libs/optimizers.lib"
+sudo_rq
 if [ ! -f "$HOME/.local/.autopatch.state" ]; then
     dsplitm_lib
 else
