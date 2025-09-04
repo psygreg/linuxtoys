a52b284dc498ef8901c5abee11fac3a1d0e6d8e8
Victor <psygreg@pm.me>
2025-09-04 13:13:12 -0300

    fix typo

--- p3/scripts/game/pup.sh
+++ p3/scripts/game/pup.sh
@@ -12,4 +12,4 @@ _lang_
 source "$SCRIPT_DIR/../../libs/lang/${langfile}.lib"
 source "$SCRIPT_DIR/../../libs/helpers.lib"
 flatpak_in_lib
-flatpak install flathub --or-update --user --noninteractive flathub net.davidotek.pupgui2
+flatpak install --or-update --user --noninteractive flathub net.davidotek.pupgui2
