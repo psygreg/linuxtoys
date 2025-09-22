7769aa5db231ee633291839220f07e5459971e38
4ntsy <228124712+4ntsy@users.noreply.github.com>
2025-09-22 19:51:29 -0300

    feat: Add Educational Apps
    Signed-off-by: 4ntsy <228124712+4ntsy@users.noreply.github.com>

---
+++ p3/scripts/education/endlesskey.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# name: Endless Key
+# version: 1.0
+# description: Curated learning anytime, anywhere
+# icon: endlesskey.png
+
+# --- Start of the script code ---
+. /etc/os-release
+source "$SCRIPT_DIR/libs/linuxtoys.lib"
+_lang_
+source "$SCRIPT_DIR/libs/lang/${langfile}.lib"
+source "$SCRIPT_DIR/libs/helpers.lib"
+flatpak_in_lib
+flatpak install --or-update --user --noninteractive flathub org.endlessos.Key
