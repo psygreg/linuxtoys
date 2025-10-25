7b8acc159366d4ff2bb79136eaaa30479f0b6f1b
Victor Gregory <psygreg@pm.me>
2025-10-24 23:46:04 -0300

    make native OBS installs for QSV work with virtual camera and browser sources

--- p3/scripts/utils/obs-intel.sh
+++ p3/scripts/utils/obs-intel.sh
@@ -12,6 +12,7 @@ source "$SCRIPT_DIR/libs/linuxtoys.lib"
 # language
 _lang_
 source "$SCRIPT_DIR/libs/lang/${langfile}.lib"
+source "$SCRIPT_DIR/libs/helpers.lib"
 # function - plugin to fix OBS mic
 obs_pipe () {
     local ver=$(curl -s "https://api.github.com/repos/dimtpap/obs-pipewire-audio-capture/releases/latest" | grep -oP '"tag_name": "\K(.*)(?=")')
@@ -27,13 +28,14 @@ obs_pipe () {
 }
 sleep 1
 sudo_rq
-_packages=(obs-studio wireplumber)
+_packages=(wireplumber)
 if is_fedora || is_ostree; then
-    _packages+=(libva-intel-media-driver)
+    rpmfusion_chk
+    _packages+=(obs-studio libva-intel-media-driver v4l2loopback)
 elif is_suse || is_debian || is_ubuntu; then
-    _packages+=(intel-media-driver)
-elif is_arch || is_cachy; then
-    _packages+=(libva-intel-driver intel-media-driver)
+    _packages+=(obs-studio intel-media-driver v4l2loopback)
+elif is_arch || is_cachy; then # get obs-studio-browser from AUR for browser source
+    _packages+=(obs-studio-browser libva-intel-driver intel-media-driver v4l2loopback-dkms)
 fi
 _install_
 sleep 1
