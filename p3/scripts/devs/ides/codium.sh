6376939e582d0274354ea195c5dd35fc7d941ef5
psygreg <psygreg@pm.me>
2026-03-20 12:10:05 -0300

    add extension fix for VSCodium

--- p3/scripts/devs/ides/codium.sh
+++ p3/scripts/devs/ides/codium.sh
@@ -10,7 +10,49 @@
 source "$SCRIPT_DIR/libs/linuxtoys.lib"
 _lang_
 source "$SCRIPT_DIR/libs/lang/${langfile}.lib"
-source "$SCRIPT_DIR/libs/helpers.lib"
-flatpak_in_lib
-flatpak install --user --or-update --noninteractive flathub com.vscodium.codium
+if is_ubuntu || is_debian; then
+    wget -qO - https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg \
+    | gpg --dearmor \
+    | sudo dd of=/usr/share/keyrings/vscodium-archive-keyring.gpg
+    echo -e 'Types: deb\nURIs: https://download.vscodium.com/debs\nSuites: vscodium\nComponents: main\nArchitectures: amd64 arm64\nSigned-by: /usr/share/keyrings/vscodium-archive-keyring.gpg' \
+    | sudo tee /etc/apt/sources.list.d/vscodium.sources
+    _packages=(codium)
+    _install_
+elif is_fedora || is_ostree; then
+    sudo tee -a /etc/yum.repos.d/vscodium.repo << 'EOF'
+[gitlab.com_paulcarroty_vscodium_repo]
+name=gitlab.com_paulcarroty_vscodium_repo
+baseurl=https://paulcarroty.gitlab.io/vscodium-deb-rpm-repo/rpms/
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg
+metadata_expire=1h
+EOF
+    _packages=(codium)
+    _install_
+elif is_suse; then
+    sudo tee -a /etc/zypp/repos.d/vscodium.repo << 'EOF'
+[gitlab.com_paulcarroty_vscodium_repo]
+name=gitlab.com_paulcarroty_vscodium_repo
+baseurl=https://paulcarroty.gitlab.io/vscodium-deb-rpm-repo/rpms/
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg
+metadata_expire=1h
+EOF
+    _packages=(codium)
+    _install_
+elif is_arch || is_cachy; then
+    _packages=(vscodium-bin)
+    _install_
+else
+    source "$SCRIPT_DIR/libs/helpers.lib"
+    flatpak_in_lib
+    flatpak install --user --or-update --noninteractive flathub com.vscodium.codium
+    flatcodium="1"
+fi
+# patch for extensions
+{ [ -n "$flatcodium" ]; wget https://raw.githubusercontent.com/psygreg/linuxtoys/master/resources/product.json -O "$HOME/.var/app/com.vscodium.codium/config/VSCodium/product.json"; } || wget https://raw.githubusercontent.com/psygreg/linuxtoys/master/resources/product.json -O "$HOME/.config/VSCodium/product.json"
 zeninf "$msg018"
