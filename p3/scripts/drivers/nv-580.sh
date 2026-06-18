4431126a93484bb77f3c8c28c57af4df922cb03e
Victor Gregory 'psygreg <psygreg@icloud.com>
2026-06-18 12:04:04 -0300

    fix #718 - v580 drivers no longer available for Debian

--- p3/scripts/drivers/nv-580.sh
+++ p3/scripts/drivers/nv-580.sh
@@ -4,36 +4,14 @@
 # icon: nvidia.svg
 # nocontainer
 # gpu: Nvidia
-# compat: debian, arch, !cachy, fedora, rhel
+# compat: arch, !cachy, fedora, rhel
 # reboot: yes
 
 # --- Start of the script code ---
 source "$SCRIPT_DIR/libs/linuxtoys.lib"
 _lang_
 sudo_rq
-if is_debian; then
-    prep_tmp
-    pkg_install gcc lsb-release
-    debian_ver=$(lsb_release -rs 2>/dev/null)
-    if [ -f /etc/apt/sources.list.d/debian.sources ]; then
-        prep_edit /etc/apt/sources.list.d/debian.sources
-        sudo sed -i 's/^Components: \(.*\)main$/Components: \1main contrib non-free/' /etc/apt/sources.list.d/debian.sources
-    else
-        prep_edit /etc/apt/sources.list
-        sudo sed -i 's/^deb http:\/\/\([^ ]*\) \([^ ]*\) main$/deb http:\/\/\1 \2 main contrib non-free/' /etc/apt/sources.list
-        sudo sed -i 's/^deb-src http:\/\/\([^ ]*\) \([^ ]*\) main$/deb-src http:\/\/\1 \2 main contrib non-free/' /etc/apt/sources.list
-    fi
-    sudo apt update
-    wget "https://developer.download.nvidia.com/compute/cuda/repos/debian${debian_ver:-trixie}/x86_64/cuda-keyring_1.1-1_all.deb"
-    pkg_fromfile cuda-keyring_1.1-1_all.deb
-    sleep 1
-    sudo apt update
-    pkg_install nvidia-driver-pinning-580 cuda-drivers-580
-    sleep 1
-    initramfs_upd
-    bootloader_upd
-    zeninf "$msg036"
-elif is_arch || is_cachy; then
+if is_arch || is_cachy; then
     pkg_install nvidia-580xx-dkms nvidia-580xx-utils nvidia-580xx-settings
     initramfs_upd
     bootloader_upd
