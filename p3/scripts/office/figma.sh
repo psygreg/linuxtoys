f990c6feea1aedf6b689f3fb998de70cc1c29451
Victor Gregory 'psygreg <psygreg@icloud.com>
2026-06-19 02:30:43 -0300

    re-enable updated Figma installer

--- p3/scripts/office/figma.sh
+++ p3/scripts/office/figma.sh
@@ -2,21 +2,14 @@
 # name: Figma
 # version: 1.0
 # description: figma_desc
-# compat: none
 # icon: figma.svg
 
 # --- Start of the script code ---
-#SCRIPT_DIR="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
 source "$SCRIPT_DIR/libs/linuxtoys.lib"
 # language
 _lang_
-source "$SCRIPT_DIR/libs/lang/${langfile}.lib"
-source "$SCRIPT_DIR/libs/helpers.lib"
-sudo_rq
-cd $HOME
-figma_tag=$(curl -s https://api.github.com/repos/Figma-Linux/figma-linux/releases/latest | grep '"tag_name"' | cut -d '"' -f4 | sed 's/^v//')
-wget https://github.com/Figma-Linux/figma-linux/releases/download/v${figma_tag}/figma-linux_${figma_tag}_linux_x86_64.AppImage
-chmod +x figma-linux-*.AppImage
-sudo ./figma-linux-*.AppImage -i
-sleep 1
-rm figma-linux-*.AppImage
+prep_tmp
+figma_ver=$(curl -s https://api.github.com/repos/arximus88/figma-linux-next/releases/latest | grep '"tag_name"' | cut -d '"' -f4 | sed 's/^v//')
+wget "https://github.com/arximus88/figma-linux-next/releases/download/v${figma_ver}/figma-linux-next_${figma_ver}_linux_x86_64.AppImage"
+pkg_appimage "figma-linux-next_${figma_ver}_linux_x86_64.AppImage"
+zeninf "$finishmsg"
